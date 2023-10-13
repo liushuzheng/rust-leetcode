9acac6b22f5f162c784ac427297f46c000b53156
liushuz <xiegutimo@aliyun.com>
2023-10-13 09:36:28 +0800

    测试 通过

--- src/simple/find_the_array_conc_val.rs
+++ src/simple/find_the_array_conc_val.rs
@@ -1,6 +1,5 @@
 /// https://leetcode.cn/problems/find-the-array-concatenation-value/
 
-use std::num;
 
 pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
     let mut start = 0;
@@ -8,7 +7,7 @@ pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
     let mut sum: i64 = 0;
     while start < end {
         let num_len = nums[end].to_string().len() as u32;
-        sum = nums[end] as i64 + nums[start] as i64 * i64::pow(10, num_len);
+        sum += nums[end] as i64 + nums[start] as i64 * i64::pow(10, num_len);
         start += 1;
         end -= 1;
     }
@@ -21,7 +20,7 @@ pub fn find_the_array_conc_val(nums: Vec<i32>) -> i64 {
 
 #[test]
 fn test_func() {
-    let v = vec![7, 52, 2, 4];
+    let v = vec![5,14,13,8,12];
     let i = find_the_array_conc_val(v);
     println!("测试返回的串联数字是{}", i);
 }
