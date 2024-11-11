5ddc76b50e8934ef71b0b9762701c20e87e012dd
liushuz <xiegutimo@aliyun.com>
2024-11-11 13:56:52 +0800

    回溯-通用解法

--- src/hello_alg/backtracking.rs
+++ src/hello_alg/backtracking.rs
@@ -1,24 +1,27 @@
-//! # 回溯算法框架 
-//! fn backtrack(state: &mut State, choices: &Vec<Choice>, res: &mut Vec<State>) {
-//!     // 判断是否为解
-//!     if is_solution(state) {
-//!         // 记录
-//!         record_solution(state, res);
-//!         // 不再继续搜索
-//!         return;
-//!     }
-//!     // 遍历所有选择
-//!     for choice in choices {
-//!         // 剪枝：判断选择是否合法
-//!         if is_valid(state, choice) {
-//!             // 尝试：做出选择，更新状态
-//!             make_choice(state, choice);
-//!             backtrack(state, choices, res);
-//!             // 回退：撤销选择，恢复到之前的状态
-//!             undo_choice(state, choice);
-//!         }
-//!     }
-//! }
+/// 通用方法
+/// # 回溯算法框架 
+///
+/// fn backtrack(state: &mut State, choices: &Vec<Choice>, res: &mut Vec<State>) {
+///     // 判断是否为解
+///     if is_solution(state) {
+///         // 记录
+///         record_solution(state, res);
+///         // 不再继续搜索
+///         return;
+///     }
+///     // 遍历所有选择
+///     for choice in choices {
+///         // 剪枝：判断选择是否合法
+///         if is_valid(state, choice) {
+///             // 尝试：做出选择，更新状态
+///             make_choice(state, choice);
+///             backtrack(state, choices, res);
+///             // 回退：撤销选择，恢复到之前的状态
+///             undo_choice(state, choice);
+///         }
+///     }
+/// }
+///
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -29,7 +32,8 @@ pub struct TreeNode {
     left: Option<Rc<RefCell<TreeNode>>>,    // 左子节点引用
     right: Option<Rc<RefCell<TreeNode>>>,   // 右子节点引用
 }
- impl TreeNode {
+
+impl TreeNode {
     /* 构造方法 */
     fn new(val: i32) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Self {
@@ -102,4 +106,64 @@ pub fn pre_order_not_3(
 }
 
 
+//通用方法
 
+/* 判断当前状态是否为解 */
+fn is_solution(state: &mut Vec<Rc<RefCell<TreeNode>>>) -> bool {
+    return !state.is_empty() && state.last().unwrap().borrow().val == 7;
+}
+
+/* 记录解 */
+fn record_solution(
+    state: &mut Vec<Rc<RefCell<TreeNode>>>,
+    res: &mut Vec<Vec<Rc<RefCell<TreeNode>>>>,
+) {
+    res.push(state.clone());
+}
+
+/* 判断在当前状态下，该选择是否合法 */
+fn is_valid(_: &mut Vec<Rc<RefCell<TreeNode>>>, choice: Option<&Rc<RefCell<TreeNode>>>) -> bool {
+    return choice.is_some() && choice.unwrap().borrow().val != 3;
+}
+
+/* 更新状态 */
+fn make_choice(state: &mut Vec<Rc<RefCell<TreeNode>>>, choice: Rc<RefCell<TreeNode>>) {
+    state.push(choice);
+}
+
+/* 恢复状态 */
+fn undo_choice(state: &mut Vec<Rc<RefCell<TreeNode>>>, _: Rc<RefCell<TreeNode>>) {
+    state.pop();
+}
+
+/* 回溯算法：例题三 */
+fn backtrack(
+    state: &mut Vec<Rc<RefCell<TreeNode>>>,
+    choices: &Vec<Option<&Rc<RefCell<TreeNode>>>>,
+    res: &mut Vec<Vec<Rc<RefCell<TreeNode>>>>,
+) {
+    // 检查是否为解
+    if is_solution(state) {
+        // 记录解
+        record_solution(state, res);
+    }
+    // 遍历所有选择
+    for &choice in choices.iter() {
+        // 剪枝：检查选择是否合法
+        if is_valid(state, choice) {
+            // 尝试：做出选择，更新状态
+            make_choice(state, choice.unwrap().clone());
+            // 进行下一轮选择
+            backtrack(
+                state,
+                &vec![
+                    choice.unwrap().borrow().left.as_ref(),
+                    choice.unwrap().borrow().right.as_ref(),
+                ],
+                res,
+            );
+            // 回退：撤销选择，恢复到之前的状态
+            undo_choice(state, choice.unwrap().clone());
+        }
+    }
+}
